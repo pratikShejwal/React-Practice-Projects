c4afd833d3f7036e03ab12973ce7a1f9ef8543a8
pratikShejwal <shejwalp147@gmail.com>
2026-06-07 01:56:46 +0530

    parcel config

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 const reactDivContainer = React.createElement('div',{id:'container'},
     [
-        React.createElement('h1',{},'heading 1'),
-        React.createElement('h2',{},'heading 2'),
+        React.createElement('h1',{key:1},'heading 1'),
+        React.createElement('h2',{key:2},'heading 2'),
     ]
 )
         const root = ReactDOM.createRoot(document.getElementById('root'))
