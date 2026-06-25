40da4bb17573b02067528043e9e0e5aaf37cff56
pratikShejwal <shejwalp147@gmail.com>
2026-06-25 21:24:27 +0530

    useState

--- src/components/ProductCart.jsx
+++ src/components/ProductCart.jsx
@@ -1,44 +1,26 @@
 import Product from "./Product";
 import productData from "../utils/FakeAPI";
+import { useState } from "react";
 const ProductCart = () =>{
- let lists = [
-    {
-    "id": 19,
-    "title": "Opna Women's Short Sleeve Moisture",
-    "price": 7.95,
-    "description": "100% Polyester, Machine wash, 100% cationic polyester interlock, Machine Wash & Pre Shrunk for a Great Fit, Lightweight, roomy and highly breathable with moisture wicking fabric which helps to keep moisture away, Soft Lightweight Fabric with comfortable V-neck collar and a slimmer fit, delivers a sleek, more feminine silhouette and Added Comfort",
-    "category": "women's clothing",
-    "image": "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_t.png",
-    "rating": {
-      "rate": 4.5,
-      "count": 146
-    }
-  },
-  {
-    "id": 20,
-    "title": "DANVOUY Womens T Shirt Casual Cotton Short",
-    "price": 12.99,
-    "description": "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
-    "category": "women's clothing",
-    "image": "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_t.png",
-    "rating": {
-      "rate": 3.6,
-      "count": 145
-    }
-  }
- ]
+
+  const [list, setList] = useState(productData)
+  
+  
+
     return (
 
         <div>
             <button onClick={()=>{
-               let li = lists.filter(product=>product.rating.rate>4);
-               console.log(li);
-               
+              // console.log(lists);
+              // console.log('filter applied');
+              let li = list.filter(product=>product.rating.rate>4);
+              // console.log(li);
+              setList(li);
             }} type="button">Top rated product</button>
         
         <div className='productCart'>
             {
-             productData.map((product,ind)=>{
+             list.map((product,ind)=>{
               return (
                 <Product key={ind} product={product}/>
             )
