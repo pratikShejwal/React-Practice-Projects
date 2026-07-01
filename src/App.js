cf26fea3a25acb1a73cca9441d02bd62af06d828
pratikShejwal <shejwalp147@gmail.com>
2026-07-02 02:58:00 +0530

    outlet

--- src/App.js
+++ src/App.js
@@ -4,26 +4,47 @@ import { jsx } from 'react/jsx-runtime'
 import NavBar  from './components/NavBar'
 import Product from './components/Product'
 import ProductCart from './components/ProductCart'
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter,Outlet,RouterProvider } from 'react-router-dom'
+import Men from './components/Men'
+import Kid from './components/Kid'
+import Women from './components/Women'
 
-const appRouter = createBrowserRouter([
-    {
-        path:'/',
-        element:<App/>
-    },{
-        path:'/kid',
-        element:<Kid/>
-    }
-
-])
 const App = ()=>{
     return (
     <>
     <NavBar/>
-    <ProductCart/>
+    <Outlet/>
     </>
     )
 }
+const appRouter = createBrowserRouter([
+    {
+        path:'/',
+        element:<App/>,
+        children:[
+            {
+                path: '/',
+                element:<ProductCart/>
+            },
+ {
+        path:'/men',
+        element:<Men/>
+    },
+    {
+        path:'/women',
+        element:<Women/>
+    },
+     {
+        path:'/kid',
+        element:<Kid/>,
+        
+    },
 
+        ],
+        errorElement:<Error/>
+    },
+   
+   
+])
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App/>);
+root.render(<RouterProvider router={appRouter}/>);
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,14 @@
+import {Link} from 'react-router-dom'
+
 const NavBar = ()=>{
     return(
     <div className='navbar'>
     <button>Light</button>
     <h1>Logo</h1>
     <ul>
-    <li>Men</li>
-    <li>Women</li>
-    <li>Kids</li>
+    <li> <Link to='/men'>Men</Link></li>
+    <li> <Link to='/women'>Women</Link></li>
+    <li> <Link to='/kid'>Kids</Link></li>
     
     </ul>
    
